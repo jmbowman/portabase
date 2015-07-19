5fd8cb7df567c2056b9801c2d7f1a666f62ea7a2
Jeremy Bowman <jmbowman@alum.mit.edu>
2015-07-19 01:38:21 -0400

    Move source code under src directory

--- packaging/copy_source.sh
+++ packaging/copy_source.sh
@@ -10,8 +10,6 @@ DEST=$1
 
 rm -rf $DEST
 mkdir -p $DEST/.tx
-cp *.h $DEST
-cp *.cpp $DEST
 cp CHANGES $DEST
 cp COPYING $DEST
 cp Doxyfile $DEST
@@ -24,20 +22,22 @@ cp portabase.iss $DEST
 cp portabase.pro $DEST
 cp portabase.rc $DEST
 cp .tx/config $DEST/.tx
-cp -R calc $DEST
-cp -R color_picker $DEST
 cp -R debian $DEST
 cp -R doc $DEST
-cp -R encryption $DEST
-cp -R image $DEST
 cp -R metakit $DEST
 rm -f $DEST/metakit/builds/*.a
 rm -f $DEST/metakit/builds/*.o
 rm -f $DEST/metakit/builds/config.*
 rm -f $DEST/metakit/builds/Makefile
-cp -R qqutil $DEST
 cp -R packaging $DEST
 cp -R resources $DEST
+cp src/*.h $DEST
+cp src/*.cpp $DEST
+cp -R src/calc $DEST
+cp -R src/color_picker $DEST
+cp -R src/encryption $DEST
+cp -R src/image $DEST
+cp -R src/qqutil $DEST
 if [ "$KEEP_HELP" = "no" ]; then
     rm -rf $DEST/resources/help/_build
 fi
