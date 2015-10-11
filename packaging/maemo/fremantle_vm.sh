6c9df5cb84ce540713c76b2f110a64a2ce5aa4b9
Jeremy Bowman <jmbowman@alum.mit.edu>
2015-10-11 01:53:38 -0400

    Automate and fix Maemo Fremantle builds

--- packaging/maemo/fremantle_vm.sh
+++ packaging/maemo/fremantle_vm.sh
@@ -1,26 +1,28 @@
 #!/bin/sh
 
-# Script to build the Diablo package for PortaBase from a Mac
-# Run this from the project root, like packaging/maemo/diablo_vm.sh
+# Script to build the Fremantle package for PortaBase from a Mac
+# Run this from the project root, like packaging/maemo/fremantle_vm.sh
 
 VERSION=`cat packaging/version_number`
-DIABLO_DIR=build/diablo
+FREMANTLE_DIR=build/fremantle
 DIR_NAME=portabase-$VERSION
-DEST=$DIABLO_DIR/$DIR_NAME
+DEST=$FREMANTLE_DIR/$DIR_NAME
 TARBALL=$DIR_NAME.tar.gz
-VM_NAME="Diablo SDK"
-SSH_PORT=3022
+VM_NAME="Fremantle SDK"
+SSH_PORT=3023
 SCRATCHBOX_HOME=/scratchbox/users/maemo/home/maemo/
 
 # Prepare the source code tarball, including translated help files
-rm -rf $DIABLO_DIR
+rm -rf $FREMANTLE_DIR
 packaging/copy_source.sh $DEST
+# Add "maemo5" to CONFIG in portabase.pro
+sed -i '' 's/warn_on/warn_on maemo5/g' $DEST/portabase.pro
 cd $DEST
 packaging/maemo/generate_help.sh
 cd ..
 tar -czf $TARBALL $DIR_NAME
 
-# Start the VM containing the Diablo SDK and copy the code into it.
+# Start the VM containing the Fremantle SDK and copy the code into it.
 # Assumes that:
 #  * /home/maemo/.ssh/authorized_keys in the VM includes your ssh public key
 #  * you've enabled port mapping in VirtualBox to allow ssh into the VM
@@ -39,18 +41,18 @@ done
 # ssh into the VM, log into scratchbox, and build the PortaBase package
 ssh -p $SSH_PORT maemo@127.0.0.1 << VM
 /scratchbox/login - << SCRATCHBOX
-sb-conf se DIABLO_ARMEL
+sb-conf se FREMANTLE_ARMEL
 rm -rf $DIR_NAME
 tar xzf $TARBALL
 rm $TARBALL
 cd $DIR_NAME
-packaging/maemo/diablo.sh
-rm -rf $DIABLO_DIR/$DIR_NAME
+packaging/maemo/fremantle.sh
+rm -rf $FREMANTLE_DIR/$DIR_NAME
 SCRATCHBOX
 VM
 
 # Copy the generated package out of the VM
-scp -r -P $SSH_PORT maemo@127.0.0.1:$SCRATCHBOX_HOME$DIR_NAME/$DIABLO_DIR build/
+scp -r -P $SSH_PORT maemo@127.0.0.1:$SCRATCHBOX_HOME$DIR_NAME/$FREMANTLE_DIR build/
 
 # Shut off the VM
 VBoxManage controlvm "$VM_NAME" poweroff
--- src/image/imagereply.cpp
+++ src/image/imagereply.cpp
@@ -1,7 +1,7 @@
 /*
  * imagereply.cpp
  *
- * (c) 2012 by Jeremy Bowman <jmbowman@alum.mit.edu>
+ * (c) 2012,2015 by Jeremy Bowman <jmbowman@alum.mit.edu>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,7 +16,7 @@
 #include <QFile>
 #include <QNetworkAccessManager>
 #include <QTimer>
-#include "database.h"
+#include "../database.h"
 #include "imagereply.h"
 
 /**
