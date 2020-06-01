422ca6d8fa8beae4119aa9a79ec3a2898487a980
Jeremy Bowman <jbowman@edx.org>
2020-05-31 23:25:15 -0400

    Update Debian packaging (#39)
    Updated the Debian packaging to work with the latest code.  Specifically:
    
    * Updated to use Qt 5
    * Updated some URLs from SourceForge to GitHub
    * Updated copyright years and file paths
    * Added the Italian translation
    * Fixed a bug finding ranlib when compiling metakit
    * Incorporated many of the patches from the official package at https://packages.debian.org/stretch/portabase (which was apparently removed from unstable because I didn't get this update done quickly enough)
    * Additional fixes and updates from a quick re-reading of the latest version of the Debian New Maintainers' Guide
    
    The package build and installation now works on the Ubuntu 18.04 system I'm currently using.

--- packaging/linux/debian.sh
+++ packaging/linux/debian.sh
@@ -13,31 +13,6 @@ rm -rf build/debian
 packaging/copy_source.sh $DEST
 cd $DEST
 
-# generate the help files here, since even unstable uses an outdated Sphinx
-rm -rf resources/help/_build/_static
-packaging/generate_help.sh en
-cd resources/help/_build
-sed -i 's:_static:../_static:g' html/*.html
-mv html/_static .
-mv html en
-cd ../../..
-for dir in `ls resources/help/translations`
-do
-    if [ "$dir" = "templates" ]; then
-        continue
-    fi
-    if [ -d "resources/help/translations/$dir" ]; then
-        packaging/generate_help.sh --no-clean "$dir"
-        cd resources/help/_build
-        sed -i 's:_static/translations:translations:g' html/*.html
-        sed -i 's:_static:../_static:g' html/*.html
-        mv html/_static/translations.js html/
-        rm -r html/_static
-        mv html "$dir"
-        cd ../../..
-    fi
-done
-
 if [ "$1" = "--source" ]; then
     dpkg-buildpackage -rfakeroot -sa -S
 else
